1bd71c8d2477dbdcddc5a4bf0b77f75e6782db01
ademidun <tomiademidun@gmail.com>
2018-08-05 19:25:49 -0400

    added team, portfolio, contact outlines

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  PROJECTS = [
+    {
+      title: 'Smart Capsule',
+      image: '../../assets/images/more-from-1.png',
+      description: 'We Helped Canada 10X dynamics put together a price estimate on' +
+      ' launching a Hyperloop type device in Dubai.',
+      slug: 'smart-capsule',
+    },
+    {
+      title: 'Smart Capsule',
+      image: '../../assets/images/more-from-2.png',
+      description: 'We Helped Canada 10X dynamics put together a price estimate on' +
+      ' launching a Hyperloop type device in Dubai.',
+      slug: 'smart-capsule',
+    },
+    {
+      title: 'Smart Capsule',
+      image: '../../assets/images/more-from-3.png',
+      description: 'We Helped Canada 10X dynamics put together a price estimate on' +
+      ' launching a Hyperloop type device in Dubai.',
+      slug: 'smart-capsule',
+    },
+  ]
+  TEAM_MEMBERS = [
+    {
+      name: 'Tomiwa Ademidun',
+      title: 'Founder, Engineering',
+      image: '../../assets/images/more-from-1.png',
+      shortBio: 'Every good boy deserves fudge.',
+      longBio: 'Senscational scenes',
+    },
+    {
+      name: 'Manpreet Matharu',
+      title: 'Engineering',
+      image: '../../assets/images/more-from-1.png',
+      shortBio: 'Every good boy deserves fudge.',
+      longBio: 'Senscational scenes',
+    },
+    {
+      name: 'Kitan Ademidun',
+      title: 'Engineering, Marketing',
+      image: '../../assets/images/more-from-1.png',
+      shortBio: 'Every good boy deserves fudge.',
+      longBio: 'Senscational scenes',
+    },
+    {
+      name: 'Zion Oginni',
+      title: 'Engineering',
+      image: '../../assets/images/more-from-1.png',
+      shortBio: 'Every good boy deserves fudge.',
+      longBio: 'Senscational scenes',
+    },
+    {
+      name: 'Valentin Kuznetcov',
+      title: 'Engineering',
+      image: '../../assets/images/more-from-1.png',
+      shortBio: 'Every good boy deserves fudge.',
+      longBio: 'Senscational scenes',
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
