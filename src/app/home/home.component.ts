572e5513cbbda84c515ac035ea3e05d3d32777d4
ademidun <tomiademidun@gmail.com>
2018-08-08 20:34:03 -0400

    refactored contact form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,15 +11,15 @@ export class HomeComponent implements OnInit {
     {
       title: 'Atila',
       image: '../../assets/images/more-from-1.png',
-      description: 'Our flaship Product. A web app that allows students to automatically' +
-      ' find and apply to scholarships at the click of a button.',
+      description: 'Our flaship Product. A web app that helps students automatically' +
+      ' find and apply to scholarships in one click.',
       projectUrl: 'https://atila.ca',
     },
     {
       title: 'Smart Capsule',
       image: '../../assets/images/more-from-2.png',
       description: 'Canada 10X Dynamics turned to Atila when they needed some mechanical and civil engineering research ' +
-      'for a cost estimate report on an innovative transportation mechanism.',
+      'for a cost estimate report on an innovative transportation mechanism in Dubai.',
     },
     {
       title: 'Smart Capsule',
@@ -33,36 +33,50 @@ export class HomeComponent implements OnInit {
       name: 'Tomiwa Ademidun',
       title: 'Founder, Engineering',
       image: '../../assets/images/team/tomiwa.jpg',
-      shortBio: 'Every good boy deserves fudge.',
+      shortBio: 'Tomiwa is currently enrolled in the dual degree software engineering' +
+      ' and business program at Ivey Business School He also works at Properly as a software developer.' +
+      'He enjoys playing soccer and is a big Arsenal F.C. fan.',
       longBio: 'Sensational scenes',
+      linkedInUrl: 'https://linkedin.com/in/tademidun',
+      profileUrl: 'https://atila.ca/profile/tomiwa',
     },
     {
       name: 'Manpreet Matharu',
       title: 'Engineering',
       image: '../../assets/images/team/manpreet.jpg',
-      shortBio: 'Every good boy deserves fudge.',
+      shortBio: 'Manpreet studied computer technology at Seneca College. \n' +
+      'Manpreet also works at RBC as a software developer.',
       longBio: 'Senscational scenes',
+      linkedInUrl: 'https://linkedin.com/in/tademidun',
+      profileUrl: 'https://atila.ca/profile/manpreet',
     },
     {
       name: 'Kitan Ademidun',
       title: 'Engineering, Marketing',
       image: '../../assets/images/team/kitan.jpg',
-      shortBio: 'Every good boy deserves fudge.',
+      shortBio: 'Kitan is currently enrolled in the dual degree computer engineering and business program at Ivey Business School \n' +
+      'She also works at TD in the data science analytics group.',
       longBio: 'Senscational scenes',
+      linkedInUrl: 'https://linkedin.com/in/tademidun',
+      profileUrl: 'https://atila.ca/profile/orekitan',
     },
     {
       name: 'Zion Oginni',
       title: 'Engineering',
       image: '../../assets/images/team/zion.jpg',
-      shortBio: 'Every good boy deserves fudge.',
+      shortBio: 'Zion studies computer science at Queen\'s University.',
       longBio: 'Senscational scenes',
+      linkedInUrl: 'https://linkedin.com/in/tademidun',
+      profileUrl: 'https://atila.ca/profile/zion',
     },
     {
       name: 'Valentin Kuznetcov',
       title: 'Engineering',
       image: '../../assets/images/team/valentine.jpg',
-      shortBio: 'Every good boy deserves fudge.',
+      shortBio: 'Valentin studied at the Ivey Business School at Western University. He is currently the CFO of ${SomeCompany}.',
       longBio: 'Senscational scenes',
+      linkedInUrl: 'https://linkedin.com/in/tademidun',
+      profileUrl: 'https://atila.ca/profile/valkuz',
     },
   ]
 
