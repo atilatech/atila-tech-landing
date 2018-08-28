2928b8fe5addf264ba4ba366d7563aaa26869d34
ademidun <tomiademidun@gmail.com>
2018-08-27 21:11:06 -0400

    listen for new swupdate using observables merge and pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { merge, Observable, of, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  updateAvailable$: Observable<boolean>;
+  closed$ = new Subject<void>();
+
+  constructor(private swUpdate: SwUpdate) {
+    this.updateAvailable$ = merge(
+      of(false),
+      this.swUpdate.available.pipe(map(() => true)),
+      this.closed$.pipe(map(() => false)),
+    );
+  }
+
+  activateUpdate() {
+    if (environment.production) {
+      this.swUpdate.activateUpdate().then(() => {
+        location.reload(true);
+      });
+    }
+  }
+
+
 }
